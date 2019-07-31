609e7742406fa46487d8aec93e77178812afa4a6
Mark Steven <aysonsteven@gmail.com>
2019-07-31 11:14:12 +0800

    Security Server

---
+++ src/main/java/guard/blog/tech/SecurityServerApplication.java
@@ -0,0 +1,11 @@
+package guard.blog.tech;
+
+import org.springframework.boot.SpringApplication;
+
+public class SecurityServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServerApplication.class, args);
+	}
+
+}
